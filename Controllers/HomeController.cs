b5479ab84f477e98f15df7c453bb7930f1ceeb36
vaibhavrja <44132251+vaibhavrja@users.noreply.github.com>
2021-09-14 12:27:45 +0530

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -20,7 +20,7 @@ namespace CICD.Controllers
 
         public IActionResult Index()
         {
-            return View()
+            return View();
         }
 
         public IActionResult Privacy()
